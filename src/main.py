62b3adb04dddc756673b944509110fa21b6eb9ba
matteo scucchia <matteo.scucchia@studio.unibo.it>
2022-11-22 11:06:18 +0100

    created package for KITTI

---
+++ src/main.py
@@ -0,0 +1,110 @@
+import argparse
+import configparser
+import sys
+
+from utils.argparse_utils import dir_path, file_path
+from datasets.KITTI_dataset import KITTIDataset
+from distances.angular_distance import angular_distance_1D
+
+import grispy as gsp
+import os
+
+
+import numpy as np
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser(
+        prog='LoopClosureInspector', 
+        description='Python tool for loop closure ground truth labeling.')
+
+    # Config File
+    parser.add_argument(
+        '--cfg', help='Configuration file path', required=True)
+
+     # Pose Graph File
+    parser.add_argument(
+        '--input', help='Input Pose Graph file path', required=True,
+        type=file_path)
+
+    # Output File
+    parser.add_argument(
+        '--output', help='Output folder to write to', required=True,
+        type=dir_path)
+
+    args = parser.parse_args()
+    
+    cfg = configparser.ConfigParser()
+    cfg.read(args.cfg)
+    return args, cfg
+
+def dataset_factory(use):
+    if use == "KITTI":
+            dataset = KITTIDataset()
+      
+    elif use == "TUM":
+        print("TUM")
+    
+    return dataset
+
+def write(output_folder, pairs, dim):
+
+    mat = np.zeros((dim, dim), dtype=np.int8)
+    f = open(os.path.join(output_folder, "pairs.txt"), "a")
+    for p in pairs:
+        mat[p[0], p[1]] = 1
+        f.write("(" + str(p[0]) + "," + str(p[1]) + ")\n")
+    f.close()
+
+    np.save("matrix.npy", mat)
+    
+    f = open(os.path.join(output_folder, "matrix.txt"), "a")
+    for row in mat:
+        f.write(str(row[0]))
+        for i in range(1, len(row)):
+            f.write(" " + str(row[i]))
+        f.write("\n") 
+            
+if __name__ == '__main__':
+    args, cfg = parse_arguments(sys.argv)
+
+    #Load poses
+    dataset = dataset_factory(cfg["settings"]["use"])
+    poses = dataset.read_file(args.input)
+
+    print(poses.shape)
+
+    #parse params for the dataset
+    section = cfg[cfg["settings"]["use"]]
+    distance_lower_bound = float(section["distance_lower_bound"])
+    distance_upper_bound = float(section["distance_upper_bound"])
+    n_frame_since_last = int(section["n_frame_since_last"])
+    translation_axis = section["translation_axis"].split(",")
+    rotation_axis = section["rotation_axis"].split(",")
+    max_angular_difference = int(section["max_angular_difference"])
+
+    translations = dataset.get_translations(poses, translation_axis)
+    angles = dataset.get_angles(poses, rotation_axis)
+
+    #euclidean distance to find the nn in a radius
+    grid_translation = gsp.GriSPy(translations, N_cells=32, metric='euclid')
+    near_translation_dist, near_translation_ind = grid_translation.shell_neighbors(translations, 
+                                                                                    distance_lower_bound=distance_lower_bound, 
+                                                                                    distance_upper_bound=distance_upper_bound)
+
+    #for the nn found, a check on the temporal distance and the angle in done, to discard false loop closures
+    good_pairs = []
+    for i, idx in enumerate(near_translation_ind):
+        if len(idx) > 1:
+            for other in idx:
+                if abs(other - i) > n_frame_since_last and angular_distance_1D(angles[i], [angles[other]], None) < max_angular_difference:
+                    good_pairs.append((i, other))
+
+    print("Found " + str(len(good_pairs)) + " correspondences!")
+
+    write(args.output, good_pairs, len(poses))
+            
+    
+    
+    
+    
---
+++ src/test.py
@@ -0,0 +1,82 @@
+import argparse
+import configparser
+import sys
+
+from utils.argparse_utils import dir_path, file_path
+from datasets.KITTI_dataset import KITTIDataset
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser(
+        prog='LoopClosureInspector', 
+        description='Python tool for loop closure ground truth labeling.')
+
+    # Config File
+    parser.add_argument(
+        '--cfg', help='Configuration file path', required=True)
+
+     # Loop Closure Pairs File
+    parser.add_argument(
+        '--input-pairs', help='Loop Closure Pairs file path', required=True,
+        type=file_path)
+
+     # Pose Graph File
+    parser.add_argument(
+        '--input-poses', help='Input Pose Graph file path', required=True,
+        type=file_path)
+
+
+    args = parser.parse_args()
+    
+    cfg = configparser.ConfigParser()
+    cfg.read(args.cfg)
+    return args, cfg
+
+def dataset_factory(use):
+    if use == "KITTI":
+            dataset = KITTIDataset()
+      
+    elif use == "TUM":
+        print("TUM")
+    
+    return dataset
+
+if __name__ == '__main__':
+    args, cfg = parse_arguments(sys.argv)
+
+    #read dataset
+    dataset = dataset_factory(cfg["settings"]["use"])
+    poses = dataset.read_file(args.input_poses)
+
+    #get translations to draw
+    translation_axis = cfg["settings"]["translation_axis"].split(",")
+    translations = dataset.get_translations(poses, translation_axis)
+
+    #read pairs
+    f_pairs = open(args.input_pairs)
+    pairs = f_pairs.readlines()
+    pairs = [pair.replace("\n", "") for pair in pairs]
+    pairs = [tuple(map(int, pair.replace("(", "").replace(")", "").split(","))) for pair in pairs]
+
+    #set indices to draw
+    size = len(poses)
+    loop_gt = np.zeros((size, size))
+
+    for pair in pairs:
+        if pair[0] != pair[1]:
+            loop_gt[pair[0], pair[1]] = 1
+
+    x = translations[:, [0]]
+    y = translations[:, [1]]
+
+    #plot the poses
+    plt.plot(x, y)
+
+    #plot the correspondences
+    indices, _ = np.where(loop_gt > 0)
+    x_coord = x[indices]
+    y_coord = y[indices]
+    plt.scatter(x_coord, y_coord, color = "red")
+    plt.show()
